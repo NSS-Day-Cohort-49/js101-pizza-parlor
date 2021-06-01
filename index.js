9e11b91ee1fbcfd79d6d5a9d6ff58f9fe1ef8b0f
Joe Shepherd <joeshepmedia@gmail.com>
2021-06-01 10:50:09 -0500

    add pizza order function

--- index.js
+++ index.js
@@ -56,36 +56,29 @@ let x = 2 + 2
 
 // ...this assigment means newId is the result of calling calcId. newId does not equal the function itself
 const newId = calcId(sizes)
-console.log(newId)
+// console.log(newId)
 
 // one way to create and populate an obj
 const pep = {}
 pep.name = "pepperoni"
 pep.price = 1.5
 pep.flavor = "greasy"
+pep.id = calcId(toppings)
 
+toppings.push(pep)
 // OR.....
 
 const blackOlives = {
   name: "black olives",
   price: 1.25,
-  flavor: {descriptions: ["gross", "yuck"]}
+  flavor: {descriptions: ["gross", "yuck"]},
+  id: calcId(toppings)
 }
 // console.log('gross?', blackOlives.flavor.descriptions[1]);
 
 // What will happen!!?
-toppings.push(pep, blackOlives)
+toppings.push(blackOlives)
 // console.log('toppings', toppings);
-
-
-
-
-
-
-
-
-
-
 
 
 // Won't cause an error. Const doesn't care about the values internal to an obj
@@ -98,3 +91,38 @@ pep.price = 5
 let foo = "hello"
 // won't cause an error
 foo = 4
+
+
+const buildPizzaOrder = (myCrust, myTopping, mySize) => {
+  const pizza = {
+    crust: myCrust,
+    topping: myTopping,
+    size: mySize,
+    totalPrice: 0
+  }
+
+  for (const crustObj of crusts) {
+    if ( crustObj.type === myCrust) {
+      pizza.totalPrice = pizza.totalPrice + crustObj.price
+    }
+  }
+
+  for (const toppingObj of toppings) {
+    if ( toppingObj.name === myTopping) {
+      pizza.totalPrice = pizza.totalPrice + toppingObj.price
+    }
+  }
+
+  for (const sizeObj of sizes) {
+    if ( sizeObj.circumference === mySize) {
+      pizza.totalPrice = pizza.totalPrice + sizeObj.price
+    }
+  }
+
+
+  return pizza
+}
+
+
+const pizza = buildPizzaOrder("deep dish", "pepperoni", 12)
+console.log(`You ordered a ${pizza.size}-inch ${pizza.crust} ${pizza.topping} pizza. Your total is $${pizza.totalPrice.toFixed(2)}`)
